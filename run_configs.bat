c508e8751cc9de08163e0445623f26a558dea551
Lucian Constantin Isac <lc.isac@lectra.com>
2025-11-23 17:57:18 +0200

    tensorboard results for configurations

--- run_configs.bat
+++ run_configs.bat
@@ -2,7 +2,7 @@
 @setlocal 
 
 set "CONFIG_DIR=D:\Fac\Fac\RN\CARN_homework_3\configuration"
-set "SCRIPT=D:\Fac\Fac\RN\pipeline.py"
+set "SCRIPT=D:\Fac\Fac\RN\CARN_homework_3\pipeline.py"
 set "EXTENSION=*.yaml"
 
 echo "Running all configs from %CONFIG_DIR% ..."
