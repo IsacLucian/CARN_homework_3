d9e1c66e01521dbde6cc4ce0a0c22380e36b63f6
Lucian Constantin Isac <lc.isac@lectra.com>
2025-11-23 17:42:02 +0200

    add script to run configurations folder

---
+++ run_configs.bat
@@ -0,0 +1,23 @@
+@echo off
+@setlocal 
+
+set "CONFIG_DIR=D:\Fac\Fac\RN\CARN_homework_3\configuration"
+set "SCRIPT=D:\Fac\Fac\RN\pipeline.py"
+set "EXTENSION=*.yaml"
+
+echo "Running all configs from %CONFIG_DIR% ..."
+echo ""
+echo "%CONFIG_DIR%\%EXTENSION%"
+for %%f in ("%CONFIG_DIR%"\"%EXTENSION%") do (
+    echo "========================================"
+    echo "Running config: %%f"
+    echo "========================================"
+
+    call python3 %SCRIPT% --config "%%f" --out "%%~nf"
+
+    echo ""
+    echo "Finished: %%f"
+    echo ""
+)
+
+echo "All experiments completed!"
